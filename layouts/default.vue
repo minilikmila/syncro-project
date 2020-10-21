5399a7acc3d43e9f694e1279fc4659658b76ce4c
Mila Shumete <minilikshumete@gmail.com>
2020-10-21 18:05:19 +0300

    customization of default.vue file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-app dark>
+  <v-app dark class="background">
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
       app
+      expand-on-hover
+      class=" "
     >
-      <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+               Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Subtitle
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+   <v-divider></v-divider>
+   <v-list nav dense>
+     <v-list-item v-for="(itemlist,index) in items"
+     :key="index"
+     link >
+       <v-list-item-icon>
+            <v-icon class="primary--text">{{ itemlist.icon }}</v-icon>
+       </v-list-item-icon>
+       <v-list-item-content>
+        <v-list-item-title @click="follow(index)" class="primary--text">{{ itemlist.title }}</v-list-item-title>
+       </v-list-item-content>
+     </v-list-item>
+   </v-list>
+    <template v-slot:append d>
+        <div>
+         <v-layout>
+            <v-btn block class="primary"> <v-icon>mdi-logout</v-icon> Logout</v-btn>
+         </v-layout>
+        </div>
+      </template>
+      <!-- <v-list>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -22,69 +52,93 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
-      </v-list>
+      </v-list> -->
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      class="green lighten-1"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="white--text"/>
+
+      <v-toolbar-title v-text="title" class="white--text" />
+
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <h3 class="white--text mr-5">Profile Name</h3>
+      <div class="text-center">
+    <v-menu offset-y
+            open-delay="1"
+            open-on-hover
+            transition="scale-transition"
+            class=""
+            light
+            >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          class="white--text"
+          v-on="on"
+          text
+        >
+          <!-- Profile -->
+          <v-icon>mdi-dots-grid</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="follow(index)"
+        >
+          <v-list-item-title class="info--text">{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+
     </v-app-bar>
-    <v-content>
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
+    </v-main>
+    <!-- <v-footer
       :fixed="fixed"
       app
+      dark
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer> -->
+     <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="grey darken-3 ">
+        <strong class="subheading font-weight-light">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 primary"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        {{ new Date().getFullYear() }} — <strong>Contacts.com</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
   </v-app>
 </template>
 
@@ -95,23 +149,36 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
+      title: "Contact.com",
       items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          { title: 'Profile Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+         icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-google-plus',
+        'mdi-linkedin',
+        'mdi-instagram',
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
+  },
+  methods: {
+    follow(index){
+      if(this.items[index].title == 'Profile Dashboard')
+       this.$router.push('/dashboard')
+      else if(this.items[index].title == 'Photos')
+       this.$router.push('/index')
+      else if (this.items[index].title == 'About')
+       this.$router.push('/signin')    }
   }
 }
 </script>
+<style scoped>
+.background{
+  /* background-color: rgba(0, 67, 89, 0.3); */
+/* background-image: url('../assets/image/test.jpg'); */
+}
+
+</style>
