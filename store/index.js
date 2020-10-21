25854a4ca936ed82cd6a6537988e6cc302a51ec4
Mila Shumete <minilikshumete@gmail.com>
2020-10-21 18:08:22 +0300

    Initial setting

---
+++ store/index.js
@@ -0,0 +1,28 @@
+// import Vue from "vue";
+// import Vuex from "vuex";
+// import VuexPersist from "vuex-persist"
+// import axios from "axios"
+
+// if (typeof window === 'undefined') {
+//   global.window = {}
+// }
+// const vuexpersist = new VuexPersist({
+//       key: "COAPP",
+//      storage: window.localStorage
+// });
+// Vue.use(Vuex);
+
+// const createStore = () =>{
+
+//    return new Vuex.Store({
+//         state: {},
+//         mutations: {},
+//         actions: {},
+//         getters: {},
+//         plugins: [vuexpersist.plugin],
+//         modules: {}
+
+
+//      });
+// };
+// export default createStore;
