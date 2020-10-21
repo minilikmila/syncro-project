233c66b2abc918f3898ec9641d80837f56caa4a3
Mila Shumete <minilikshumete@gmail.com>
2020-10-21 18:03:59 +0300

    setting the header of each pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  mode: 'universal',
+  mode:'universal',
   /*
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | ' + process.env.npm_package_name,
+    title: 'contacts' || process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
