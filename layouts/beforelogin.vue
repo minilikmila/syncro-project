ff184f372d8660c075ecd3aecfaf306860b38ce2
Mila Shumete <minilikshumete@gmail.com>
2020-10-24 18:14:35 +0300

    create for layout befor logged in page

---
+++ layouts/beforelogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-app-bar fixed
+                    app>
+           <v-toolbar-title class="info--text ml-12">Contact.com</v-toolbar-title>
+           <div class="drawerstyle success--text">
+             Thanks for choosing us!
+           </div>
+        </v-app-bar>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+.drawerstyle{
+  margin-left: 30%;
+}
+</style>
