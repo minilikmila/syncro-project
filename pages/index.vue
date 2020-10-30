051fe48378a41cb962d83a91aa37828e3c039db8
Mila Shumete <minilikshumete@gmail.com>
2020-10-30 16:19:59 +0300

    setting up dashboard with modified features

--- pages/index.vue
+++ pages/index.vue
@@ -1,94 +1,56 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <!-- <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div> -->
-      <v-card flat width="800px" height="500px" class="mt-10">
-        <v-card-title class="headline">
-          Welcome to our contact registration service. we glad to know u.
-        </v-card-title>
-        <v-card-text>
-          <h3 class="success--text"> This is Minilik </h3>
-          <!-- <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a> -->
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/signin"
-            class="mt-10"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-container>
+    <!-- -->
+  <v-layout class="general">
+    <v-flex lg6 md6 sm6 class="white--text darken-3 mr-10">
+     <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur qu
+       is dolorum, architecto mollitia recusandae eum tempora illum, blan
+       ditiis ipsum molestiae est obcaecati voluptatibus autem, unde quasi amet quidem culpa dignissimos.</p> -->
+      <strong class="ml-12">services provided</strong>
+         <v-divider class="white"></v-divider>
+      <ul class="mt-8">
+        <li>You can upload contacts picture. </li>
+      </ul>
+      <h3 class="mt-7 font-italic font-weight-light">If you need to login right now
+        <nuxt-link to="/signin" class="warning--text darken-3">click here</nuxt-link> </h3>
+      <h3 class="mt-8 font-italic font-weight-light">but if you haven't accout yet please
+        <nuxt-link to="/signup" class="warning--text darken-3">Click here now</nuxt-link>
+      </h3>
+    </v-flex>
+    <v-flex lg6 md6 sm6 class="white--text darken-3">
+        <strong class="ml-12"> Read to get more informations </strong>
+       <v-divider class="white"></v-divider>
+       <ul class="mt-7">
+         <li>This service provides 15GB for your contact registration</li>
+         <li>If you have not an account yet you have to sign up and join the service</li>
+         <li>Before you sign up please check you have an email address bcs its mandatory</li>
+       </ul>
+       <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, qua
+         m inventore explicabo necessitatibus distinctio s
+         equi ullam officiis temporibus iste aliquid ex cum illo fugit eius dolorem quasi. Deserunt, ab culpa?</p> -->
+
     </v-flex>
   </v-layout>
+  </v-container>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+  export default {
+    layout: 'homelayout',
+    data () {
+      return {
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+      }
+    },
   }
-}
 </script>
+<style scoped>
+.general{
+  /* width: 1000px; */
+    margin-top: 6%;
+    opacity: 0.9;
+}
+.btnstyle{
+  margin-left: 40%;
+  margin-top: 20%;
+}
+</style>
