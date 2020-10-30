0985551ce7b2e574423ee820eb597b8407f7c40b
Mila Shumete <minilikshumete@gmail.com>
2020-10-30 16:20:38 +0300

    minor setting commited

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
-    <v-card class="cardstyle" width="50%">
+    <v-card class="cardstyle" width="50%" @keyup.enter="signin">
       <v-card-title class="signin-title">
         <h2 class="primary--text ml-10 headerline">Sign In</h2>
-      </v-card-title>
+      </v-card-title> <v-divider></v-divider>
       <!-- <hr> -->
-      <v-card-text class="mt-10">
+      <v-card-text class="mt-10" ref="form">
         <v-form>
-          <v-text-field label="Username"  @focus="warn= false" v-model="username"></v-text-field>
-          <v-text-field label="Password" type="password"  @focus="warn= false" v-model="password" ></v-text-field>
+          <v-text-field label="Username"  @focus="warn= false" v-model="username" outlined
+                        prepend-inner-icon="mdi-account"></v-text-field>
+          <v-layout>
+             <v-text-field label="Password" :type="type"  @focus="warn= false" v-model="password" outlined
+                        prepend-inner-icon="mdi-key"  ></v-text-field>
+                        <v-icon @click="showPassword" class="passEye info--text">{{eyeIcon}}</v-icon>
+          </v-layout>
            <p class="subtitle-1 red--text font-italic"
               v-show="warn">U use incorrect password or username please try again correctly!</p>
         </v-form>
@@ -22,8 +27,8 @@
       </v-card-actions>
      </v-card>
      <div class="signupbtn mt-5">
-      <p class="">If You haven't account yet; u can register here
-      <v-btn text class="primary--text" @click="$router.push('/signup')">signUp</v-btn></p>
+      <p class="">If You haven't account yet; please
+      <nuxt-link to="/signup">Click here </nuxt-link>  </p>
      </div>
   </v-container>
 </template>
@@ -37,10 +42,22 @@ export default {
       username: '',
       password: '',
       warn: false,
+       eyeIcon: 'mdi-eye',
+       type: 'password',
       // accountInfo: {}
     }
   },
   methods: {
+    // Show password or not
+    showPassword(){
+       if(this.type == 'password'){
+         this.type = '';
+         this.eyeIcon = 'mdi-eye-off';
+       } else {
+            this.type = 'password';
+            this.eyeIcon= 'mdi-eye';
+       }
+    },
     // signin request!!
        signin(){
 
@@ -59,9 +76,10 @@ export default {
             this.$store.commit('setUsername', response.username);
             this.$store.commit('setToken', response.token);
             this.$store.commit('setEmail', response.email);
-            this.$store.commit('setFull_name', response.F_name, response.L_name)
+            this.$store.commit('setFull_name', response.F_name, response.L_name);
+            this.$store.commit('setImageUrl', response.imageUrl);
             this.$store.commit('setSocial_media', response.social_media);
-            this.$router.push('/dashboard');
+            this.$router.push('/dashboard/' + response.id + '?access_token='+ response.token);
             //  console.log("from social testing")
             //  console.log(this.$store.getters.social_media);
             }
@@ -92,5 +110,9 @@ export default {
 }
 .signupbtn{
   margin-left: 55%;
+}
+.passEye{
+  margin-left: -7%;
+  margin-bottom: 4%;
 }
 </style>
